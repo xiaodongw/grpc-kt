21197884eebb12e6155fbcc83db0a27c424693b0
xiaodong Wang <xiaodongw79@gmail.com>
2019-03-05 13:01:03 -0800

    unignore build scripts.

---
+++ buildSrc/src/main/kotlin/build/Deps.kt
@@ -0,0 +1,11 @@
+package build
+
+@Suppress("unused")
+object Deps {
+  const val kotlinVersion = "1.3.21"
+  const val kotlinCoroutineVersion = "1.1.1"
+  const val protobufVersion = "3.6.1"
+  const val junitVersion = "5.3.2"
+  const val mockitoVersion = "2.24.0"
+  const val grpcJavaVersion = "1.17.1"
+}
