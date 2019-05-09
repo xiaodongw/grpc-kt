6ab4b696df7f9d42b690a50294a0f3db4e6adde4
Xiaodong Wang <xiaodongw79@gmail.com>
2019-05-08 18:58:14 -0700

    dependency: upgrade to latest versions.

--- buildSrc/src/main/kotlin/build/Deps.kt
+++ buildSrc/src/main/kotlin/build/Deps.kt
@@ -2,10 +2,10 @@ package build
 
 @Suppress("unused")
 object Deps {
-  const val kotlinVersion = "1.3.21"
-  const val kotlinCoroutineVersion = "1.1.1"
-  const val protobufVersion = "3.6.1"
-  const val junitVersion = "5.3.2"
-  const val mockitoVersion = "2.24.0"
-  const val grpcJavaVersion = "1.17.1"
+  const val kotlinVersion = "1.3.31"
+  const val kotlinCoroutineVersion = "1.2.1"
+  const val protobufVersion = "3.7.1"
+  const val junitVersion = "5.4.1"
+  const val mockitoVersion = "2.27.0"
+  const val grpcJavaVersion = "1.20.0"
 }
